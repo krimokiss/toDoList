494a759cb4f9be97729a2d7ae8f2c271fa270d40
Abdelkrim <kissab@gmail.com>
2023-03-13 15:53:49 +0100

    no

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,3 @@
-// export const environment = {
-//   production: true
-// };
 export const environment = {
-  // API_URL : 'https://easycontratserver.onrender.com/api',
-    API_URL : 'https://to-do-list-server-six.vercel.app/',
   production: true
 };
